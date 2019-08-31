160e5de43ca007131dd75993b5a734a0be6fcfe8
vagrant <vagrant@homestead>
2019-08-31 08:26:35 +0000

    众筹商品详情页

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
+    Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
