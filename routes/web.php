b39d07d39f72b63b16dd2a90f9a45c48a8cb7e1a
vagrant <vagrant@homestead>
2019-09-08 06:00:03 +0000

    前台分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     //分期付款下单
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
+
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
